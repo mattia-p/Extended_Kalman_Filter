132fcbfc4bc66032230adec3096f3cc0a752764a
Mattia <mattia_pelissou@berkeley.edu>
2020-04-19 16:47:07 -0700

    compute rmse for performance evaluation

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,9 +11,27 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+
+    // initialize the rmse vector
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+   if (estimations.size() == 0 || estimations.size() != ground_truth.size()){
+       cout << "Estimation or ground truth data invalid" << endl;
+   }
+
+   for (int i = 0; i < estimations.size(); i++){
+       VectorXd residual = estimations[i] - ground_truth[i];
+       // multiplication term by term
+       residual = residual.array()*residual.array();
+       rmse += residual;
+   }
+
+   // calculate the mean for each component of the RMSE
+   rsme = rme/estimations.size();
+
+   // calculate the squared root
+   rmse = rmse.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
