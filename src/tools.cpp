5204916c88081c3e3b7dde294b3a3bf52a760d61
Mattia <mattia_pelissou@berkeley.edu>
2020-04-19 17:06:35 -0700

    compute jacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -35,8 +35,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+    // jacobian
+    Hj = MatrixXd(3,4);
+
+    float px = x_state[0];
+    float py = x_state[1];
+    float vx = x_state[2];
+    float vy = x_state[3];
+
+    // precompute some variables to avoid repetitions
+    float c1 = px * px + py * py;
+    float c2 = sqrt(c1);
+    float c3 = (c1*c2);
+
+    // check if division by zero
+    if (fabs(c1) < 0.0001){
+        cout << "Error in computing jacobian - division by zero" << endl;
+        return
+    }
+
+
+    Hj << px / c2, py/c2, 0, 0,
+          -py/c1, px/c1, 0, 0,
+          py*(vx*py - vy*px)/c2, px*(vy*px - vx*py)/c3, px/c2, py/c2;
+
+    return Hj;
 }
