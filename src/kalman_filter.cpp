c3eb0c874ec2d2fbc476400769628b48a5138b2a
Mattia <mattia_pelissou@berkeley.edu>
2020-04-26 22:04:31 -0700

    Change predict and update functions

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,18 +23,14 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+
    x_ = F_ * x_;
    MatrixXd Ft = F_.transpose();
    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+ 
    VectorXd z_pred = H_ * x_;
    VectorXd y = z - z_pred;
    MatrixXd Ht = H_.transpose();
